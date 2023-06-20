c5dc43cecf0162b78ca8cc39e450ee110dda3443
0xBugatti <110223394+0xBugatti@users.noreply.github.com>
2023-06-20 07:12:45 -0400

    Add files via upload

---
+++ PowerShellRunner/PowerShellRunner/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Linq;
+using System.Globalization;
+using System.Reflection;
+using System.IO.Compression;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Diagnostics;
+
+namespace PowerShellRunner
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var proc = new Process();
+            System.Diagnostics.ProcessStartInfo startInfo = new System.Diagnostics.ProcessStartInfo();
+            startInfo.WindowStyle = System.Diagnostics.ProcessWindowStyle.Hidden;
+            //EXE
+            startInfo.FileName = "cmd.exe";
+            //Arguments
+            startInfo.Arguments = "/c" + "curl http://127.0.0.1:2100/ ";
+            proc.StartInfo = startInfo;
+            proc.Start();
+            
+
+        }
+    }
+}
---
+++ Reflection-Beta/ConsoleApp/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+          static Int64 maina()
+        {
+
+
+
+            return 12;
+        }
+        static void Main(string[] args)
+        {
+
+
+
+
+            //Reflection
+          
+
+            int e = 47;
+            Type t0 = e.GetType();
+            Console.WriteLine(t0);
+            Type t2 = Type.GetType("System.Int64");
+/*            var dll = Assembly.LoadFrom(@"C:\Users\Blu-Ray\source\testing\DotNetDLL\DotNetDLL\bin\Debug\DotNetDLL.dll");
+            Type t1 = dll.GetType();*/
+            Type t3 = typeof(UInt16);
+            Console.WriteLine(t2);
+
+            Console.WriteLine($"DllName is :{t2.Assembly}");
+            Console.WriteLine($"Dll is GAC :{t2.Assembly.GlobalAssemblyCache}");
+            Console.WriteLine($"Name Space is :{t2.Namespace}");
+            Console.WriteLine($"ClassName is :{t2.Name}");
+            Console.WriteLine($"FullName is :{t2.FullName}");
+
+            MethodInfo[] methods = t2.GetMethods();
+            Console.WriteLine($"Number Of Methods is {methods.Length}");
+            foreach (var method in methods) { Console.WriteLine($"Found method: {method}"); }
+
+            
+            PropertyInfo[] Properties = t2.GetProperties();
+            Console.WriteLine($"Number Of Properties is {Properties.Length}");
+            foreach (var property in Properties) { Console.WriteLine($"Found method: {property}"); }
+
+            
+
+
+
+
+
+            object classInstance = Activator.CreateInstance(typeof(int));/*
+            int aas = int.Parse("12");
+
+
+
+
+            //Type[] parameterType = {typeof(string) };
+
+
+            object magicValue = methods[11].Invoke(classInstance, new object[] { "100" });
+            */
+            
+            
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            
+
+        }
+    }
+}
